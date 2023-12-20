b6dc89fd4da15b0a2989c964b1e15766d0e2a9b4
Andrii Protas <andreyprotas5@gmail.com>
2023-12-20 22:49:47 +0100

    Added User entity

---
+++ src/main/java/com/andriiiiiko/spring/entity/User.java
@@ -0,0 +1,33 @@
+package com.andriiiiiko.spring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
